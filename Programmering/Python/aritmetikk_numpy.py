6af7b8bdcd91d91ec9d06935ddfe5fac2900ad80
krisbheiLink <krisbhei@ifi.uio.no>
2018-01-20 23:43:48 +0100

    numpy-demo

---
+++ Programmering/Python/aritmetikk_numpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Lage to 1 x 3 matriser:
+a = np.array([1,2,3])
+b = np.array([11,12,13])
+
+# elementvis addisjon
+print("a+b =",a+b)
+
+# elementvis multiplikasjon
+print("a*b =",a*b)
