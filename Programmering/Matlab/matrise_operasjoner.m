88093e1123a5b45bbf6b24bbdb2ec3f6fd37ee13
krisbheiLink <krisbhei@ifi.uio.no>
2018-01-21 13:05:38 +0100

    mat-filer

---
+++ Programmering/Matlab/matrise_operasjoner.m
@@ -0,0 +1,25 @@
+A = [1 2 3; 4 5 6];
+a = 3;
+
+% Addér hvert element i A med a
+A + a
+
+% Subtrahér hvert element i A med a
+A - a
+
+% Multiplisér hvert element i A med a
+A.*a
+
+% Del hvert element i A på a
+A./a
+
+% Opphøy hvert element i A med a
+A.^a
+
+% Transponér A (dvs, gjøre om radene i A til kolonner)
+A.'
+
+B = [10 11;12 13;14 15];
+
+% Matrisemultiplikasjon
+A*B
