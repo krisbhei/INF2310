3d34b80ab5e4a40bd626f212fce5e1df82acb97c
krisbheiLink <krisbhei@ifi.uio.no>
2018-01-21 01:07:55 +0100

    framvising

--- Programmering/Python/fremvising.py
+++ Programmering/Python/fremvising.py
@@ -3,7 +3,12 @@ import matplotlib.pyplot as plt
 
 img = imread("houses.png",flatten=True)
 
+plt.imshow(img)
+plt.title("Et gråtonebilde?")
+
+plt.figure() # For å lage et nytt vindu
+
 plt.imshow(img,cmap='gray')
-plt.title("En tittel")
+plt.title("Et gråtonebilde")
 
 plt.show() # Husk dette for å faktisk se visualiseringene!
