c1ba8f627f10c275835f36ac664a682e695ba03b
krisbheiLink <krisbhei@ifi.uio.no>
2018-02-17 15:28:38 +0100

    u4-Matlab

---
+++ Uke4/Matlab/finn_histogram_bilde.m
@@ -0,0 +1,26 @@
+
+% finn_histogram_bilde(img)
+% Finner det normaliserte og kumulative histogrammet til innbildet img
+%
+% Argument:
+%   * img: Bildet som funksjonen skal finne histogrammene til. 
+%          Bildet antas å være i 8-bit
+% Returnerer:
+%   * [p,c]: p er normalisert histogram og c er det kumulative
+%            histogrammet.
+function [p,c] = finn_histogram_bilde(img)
+    G = 256;
+    p = zeros(1,G);
+    
+    for i=1:G
+        p(i) = sum(sum(img == i-1));
+    end
+    p = p./numel(img);
+    
+    c = zeros(1,G);
+    c(1) = p(1);
+    for i=2:G
+        c(i) = c(i-1) + p(i);
+    end
+    
+end
---
+++ Uke4/Matlab/oppgave3.m
@@ -0,0 +1,13 @@
+G = 256;
+intensiteter = linspace(0,G-1,G);
+
+img = imread('mona.png');
+
+uniform = @(G) ones(1,G)./G;
+
+q = uniform(G);
+q = q./sum(q);
+
+img_T = histogramtilpasning(img,q);
+
+plot_histogramtransformert(img,img_T,q)
